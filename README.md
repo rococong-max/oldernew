85c499a8435a96f74f7dc1c968d74b8a8e83ff53
rococong-max <rococong@gmail.com>
2026-04-13 18:15:19 +0900

    header, fab, herobnr

--- README.md
+++ README.md
@@ -21,4 +21,31 @@
 * html 내용 수정
 * 이미지 파일 채우기
 ### 2026-04-09
-* 
+* top_swiper - css,js (오작동)
+### 2026-04-10
+* top_swiper - css,js (작동)
+* 이미지 src 주소 작성 완료
+* header css 시작
+### 2026-04-13
+* header css 작성 완료
+    * 추가 작업 필요 : 웹 상황에 따라 `#menu_btn` 보임, `#menu_btn` 클릭 시 `#side_menu` 보임, `#side_menu` css 작성
+* fab_wrap css 작성 및 js 구현 완료
+* hero_wrap css 작성 및 js 통해 swiper 구현 완료
+* fab_wrap css가 안먹음.. 개발자도구에서 아예 안보임..
+
+## 헷갈리는 개념 정리
+### 반응형 웹 - 환경 변화에 따른 header 레이아웃 변화
+* desktop,tablet -> gnb 바로 보임, 햄버거메뉴 x
+* mobile -> gnb 바로 안보임, 햄버거메뉴 O
+=> `@media` 미디어쿼리를 사용해서 display : none 또는 block; 속성으로 구현
+### 햄버거 메뉴를 눌렀을 때 사이드 메뉴 나타남
+* js를 사용해서 구현
+
+
+## 자주하는 실수 정리
+### html
+### css
+* fixed는 position의 값
+### js
+* `classList.add('show')` (O)
+* `classList.add('.show')` (X) 안에 클래스 명을 쓸 때는 .쓰지 않음
--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,34 @@
 // index.js
 
-// 띠배너
-// DOM
+// DOM - html 태그 선택
 const topBnr = document.querySelector('#top_bnr .top_bnr_swiper');
+const topBtn = document.querySelector('#fab_wrap .top_btn');
+const heroBnr = document.querySelector('#hero_bnr_wrap .hero_bnr_swiper')
 
-console.log(topBnr);
+console.log(topBnr,topBtn);
 
-// swiper
+// 띠배너 swiper
 const topBnrSwiper = new Swiper(topBnr,{
     loop:true,
     autoplay:{delay:2000,disableOnInteraction: false,},
     direction:'vertical',
+    speed:800,
+})
+// 히어로배너 swiper
+const heroBnrSwiper = new Swiper(heroBnr,{
+    loop:true,
+    autoplay:{delay:3000,},
+    disableOnInteraction: false,
+    speed:800,
+})
+
+
+// 화면이 일정 길이 만큼 스크롤 되면 top_btn에 .show 클래스명 주기
+window.addEventListener('scroll',function(){
+    // scrollY가 200보다 크면 'show' 클래스 추가, 아니면 제거
+    if(scrollY > 200){
+        topBtn.classList.add('show');
+    } else {
+        topBtn.classList.remove('show');
+    }
 })
