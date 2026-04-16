a2eaacdf22a69fb70e5b5623620669816d4032f0
rococong-max <rococong@gmail.com>
2026-04-16 18:12:50 +0900

    신제품 영역 js까지 완료, 맞춤추천 css+js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
 // index.js
 
-// DOM - html 태그 선택
+// 변수
 const topBnr = document.querySelector('#top_bnr .top_bnr_swiper');
 const topBtn = document.querySelector('#fab_wrap .top_btn');
 const heroBnr = document.querySelector('#hero_bnr_wrap .hero_bnr_swiper');
-const newProd = document.querySelector('.new_product .new_swiper');
+const newProd = document.querySelectorAll('.new_product .new_swiper');
+const clearTab = document.querySelector('.new_title_tab .clear');
+const comfortTab = document.querySelector('.new_title_tab .comfort');
+const clearContainer = document.querySelector('#new_wrap .clear_container');
+const comfortContainer = document.querySelector('#new_wrap .comfort_container');
+const onceDayProd = document.querySelector('#recommendation_wrap .once_day_swiper');
+const vLineProd = document.querySelector('#recommendation_wrap .v_line_swiper');
+const modeProd = document.querySelector('#recommendation_wrap .mode_swiper');
+const onceDayTab = document.querySelector('#recommendation_wrap .once_day')
+const vLineTab = document.querySelector('#recommendation_wrap .v_line')
+const modeTab = document.querySelector('#recommendation_wrap .mode')
 
-console.log(topBnr,topBtn,newProd);
+console.log(topBnr,topBtn,newProd,clearTab,comfortTab,clearContainer,comfortContainer,onceDayProd);
+console.log(onceDayTab,vLineTab,modeTab);
 
 // FAB
 // 화면이 일정 길이 만큼 스크롤 되면 top_btn에 .show 클래스명 주기
@@ -39,8 +50,72 @@ const heroBnrSwiper = new Swiper(heroBnr,{
 })
 
 // 신제품 상품 이미지 swiper
-const newSwiper = new Swiper(newProd,{
+const clearSwiper = new Swiper(newProd[0],{
     loop:true,
     autoplay:{delay:3000,},
     slidesPerView:1.8,
 })
+const comfortSwiper = new Swiper(newProd[1],{
+    loop:true,
+    autoplay:{delay:3000,},
+    slidesPerView:1.8,
+})
+
+// 신제품 탭 메뉴 전환
+// 클릭하면 함수 실행 (이벤트)
+comfortTab.addEventListener('click',comfortShow)
+clearTab.addEventListener('click',clearShow)
+// comfort 탭 클릭 시 실행되는 함수
+function comfortShow(e){
+    e.preventDefault();
+    // active 클래스 제거 -> 클리어 내용 숨기기 -> 컴포트에 active 주기 -> 컴포트 내용 보임
+    clearTab.classList.remove('active');
+    clearContainer.style.display = 'none';
+    comfortTab.classList.add('active');
+    comfortContainer.style.display = 'flex';
+}
+// clear 탭 클릭 시 실행되는 함수
+function clearShow(e){
+    e.preventDefault();
+    // active 클래스 제거 -> 컴포트 내용 숨기기 -> 클리어에 active 주기 -> 클리어 내용 보임
+    comfortTab.classList.remove('active');
+    comfortContainer.style.display = 'none';
+    clearTab.classList.add('active');
+    clearContainer.style.display = 'flex';
+}
+
+// 추천 영역 상품 swiper (모바일 상황에서만 swiper 작동되도록)
+const onceDaySwiper = new Swiper(onceDayProd,{
+    spaceBetween:28,
+    slidesPerView:3,
+})
+const vLineSwiper = new Swiper(vLineProd,{
+    spaceBetween:28,
+    slidesPerView:3,
+})
+const modeSwiper = new Swiper(modeProd,{
+    spaceBetween:28,
+    slidesPerView:3,
+})
+
+// 추천 영역 탭 전환
+// 클릭하면 함수 실행 (이벤트)
+onceDayTab.addEventListener('click',onceDayShow);
+vLineTab.addEventListener('click',vLineDayShow);
+modeTab.addEventListener('click',modeShow);
+// 함수
+// onceday 클릭
+function onceDayShow(e){
+    e.preventDefault();
+    
+}
+// vline 클릭
+function vLineDayShow(e){
+    e.preventDefault();
+
+}
+// mode 클릭
+function modeShow(e){
+    e.preventDefault();
+
+}
