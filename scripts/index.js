07a6e917c3534c4f01a690d05b4bfa870cf0193b
rococong-max <rococong@gmail.com>
2026-04-17 20:30:37 +0900

    메인페이지 90% 정도 완료

--- scripts/index.js
+++ scripts/index.js
@@ -13,12 +13,12 @@ const onceDayProd = document.querySelector('#recommendation_wrap .once_day_swipe
 const vLineProd = document.querySelector('#recommendation_wrap .v_line_swiper');
 const modeProd = document.querySelector('#recommendation_wrap .mode_swiper');
 const recProd = document.querySelectorAll('#recommendation_wrap .swiper');
-const onceDayTab = document.querySelector('#recommendation_wrap .once_day');
-const vLineTab = document.querySelector('#recommendation_wrap .v_line');
-const modeTab = document.querySelector('#recommendation_wrap .mode');
+const recTab = document.querySelectorAll('#recommendation_wrap .tab a');
+const reviewList = document.querySelector('.review_swiper_wrap .review_swiper');
+const magazineList = document.querySelector('#magazine_wrap .magazine_swiper')
 
 console.log(topBnr,topBtn,newProd,clearTab,comfortTab,clearContainer,comfortContainer,onceDayProd);
-console.log(onceDayTab,vLineTab,modeTab);
+console.log(/* onceDayTab,vLineTab,modeTab */recTab);
 
 // FAB
 // 화면이 일정 길이 만큼 스크롤 되면 top_btn에 .show 클래스명 주기
@@ -30,6 +30,12 @@ window.addEventListener('scroll',function(){
         topBtn.classList.remove('show');
     }
 })
+// top버튼 눌렀을 때 상단으로 이동
+topBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    window.scrollTo(0,0)
+})
+
 
 // 띠배너 swiper
 const topBnrSwiper = new Swiper(topBnr,{
@@ -85,7 +91,8 @@ function clearShow(e){
     clearContainer.style.display = 'flex';
 }
 
-// 추천 영역 상품 swiper (모바일 상황에서만 swiper 작동되도록)
+// 추천 영역 상품 swiper (모바일 상황에서만 swiper 작동되도록).
+
 const recSwiperCommon = {
     spaceBetween:28,
     slidesPerView:3,
@@ -98,11 +105,43 @@ const modeSwiper = new Swiper(modeProd,recSwiperCommon)
 
 
 // 추천 영역 탭 전환
-// 1. 탭 안 컨텐츠 다 안보이게 하고
-// 2. 개별 인덱스로 해당 컨텐츠만 보이도록 함, 해당 탭 활성화 -> .active로 조절 
-// 3. 클릭 이벤트로 연결
-function recShowHide(index){
-    for (let i = 0; i < recProd.length; i++){
-        
+// 최종 목표 : 탭(recTab)을 클릭했을 때 모든 내용 숨기고 클릭한 탭과 연결된 내용만 보이도록 하기 (active활용)
+// 모두에게 active속성 제거 -> 클릭한 해당 탭과 컨텐츠에 active 속성 부여
+function tabClickFunc(e){
+    e.preventDefault();
+    const targetI = e.currentTarget.dataset.index;
+    // active 제거
+    for(let i = 0; i < recTab.length; i++){
+        recTab[i].classList.remove('active');
+        recProd[i].classList.remove('active');
     }
-} 
+    // 클릭된 탭과 컨텐츠에 active속성 주기
+    recTab[targetI].classList.add('active');
+    recProd[targetI].classList.add('active');
+}
+// 위 함수가 탭이 클릭했을 때 실행
+recTab[0].addEventListener('click', tabClickFunc);
+recTab[1].addEventListener('click', tabClickFunc);
+recTab[2].addEventListener('click', tabClickFunc);
+
+// best review
+// swiper
+const reviewSwiper = new Swiper(reviewList,{
+    spaceBetween:32,
+    slidesPerView: 5,
+    centeredSlides:true,
+    loop:true,
+    navigation:{
+        prevEl:'.review_title_btn .btn_box .prev',
+        nextEl:'.review_title_btn .btn_box .next',
+    },
+})
+
+// magazine
+// swiper
+const magazineSwiper = new Swiper(magazineList, {
+    spaceBetween:12,
+    slidesPerView: 5,
+    centeredSlides:true,
+    loop:true,
+})
